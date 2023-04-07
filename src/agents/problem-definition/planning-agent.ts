d14cb27394e8f61bc1268c4262f0ebda3a18cf87
Martin Høst Normark <martin.hoest.normark@lego.com>
2023-04-07 17:54:10 +0200

    Create planning-agent.ts

---
+++ src/agents/problem-definition/planning-agent.ts
@@ -0,0 +1,12 @@
+// Based on the issue, define details via prompts:
+// - What is the problem?
+// - Which code needs to be changed?
+// - If this is a new code file, where should it be located
+// - Get a tree view of the code-base and use in prompts (tree view command or source path should be configurable)
+// - Where do we store the tests
+// - Get the command to create the file
+// - Get the command to create the test file
+// - Report back progress to the issue
+
+// Where do we store state? A file that is never committed?
+// How do we steer the model for awareness of the codebase and perhaps knowledge from PRs etc?
